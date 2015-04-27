5f2d957b1671fceed1793e0e0db5d9a97cba524c
Damian Spinek <damian00737@gmail.com>
2015-04-27 10:07:56 +0200

    Utworzenie logiki klasy BetterRadar
    Klasa ta przenosi odpowiedzialnosc za wykonanie watku do obiektu executora

--- src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,5 +1,33 @@
 package edu.iis.mto.multithread;
 
-public class BetterRadar {
+import java.util.concurrent.Executor;
 
+public class BetterRadar {
+	private PatriotBattery battery;
+	private Executor launchExecutor;
+
+	public BetterRadar(PatriotBattery missle) {
+		this.battery = missle;
+	}
+
+	public void notice(Scud enemyMissle) {
+		launchPatriot();
+	}
+
+	private void launchPatriot() {
+		Runnable launchPatriotTask = new Runnable() {
+			public void run() {
+				for (int i = 0; i < 10; i++) 
+					battery.launchPatriot();
+				
+			}
+		};
+		launchExecutor.execute(launchPatriotTask);
+	}
+
+
+
+	public void setLaunchExecutor(Executor launchExecutor) {
+		this.launchExecutor = launchExecutor;
+	}
 }
