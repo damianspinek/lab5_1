4772de2ce25d6417c7fcbbdcc5aebbc82486b238
Damian Spinek <damian00737@gmail.com>
2015-04-27 10:18:50 +0200

    Umozliwienie wielokrotnego wykonywania testow w klasie BetterRadarTest poprzez adnotacje Repeat
    Dodanie mechanizmu obslugi adnotacji repeat i role

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -5,10 +5,18 @@ import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.Executor;
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import edu.iis.mto.multithread.RepeatRule.Repeat;
+
 public class BetterRadarTest {
+	
+	@Rule
+	public RepeatRule repeatRule = new RepeatRule();
+	
 	@Test
+	@Repeat( times = 100000 )
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
 		Executor launchExecutor = new LaunchPatriotExecutor();
